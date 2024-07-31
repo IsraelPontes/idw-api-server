30480bb9580c107e89707f259a9d11d0223e88ac
IsraelPontes <israelcpontes@hotmail.com>
2024-07-31 16:40:48 -0300

    Configuração do CORS

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import { PrismaClient } from '@prisma/client'
 
 const app = express()
 const prisma = new PrismaClient()
 app.use(express.json())
+app.use(cors('https://idw-server-api.netlify.app/'))
 
 const users = []
 
